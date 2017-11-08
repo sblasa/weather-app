b80260a4bb74535fcbb904fe920c95bd70532ac3
sblasa <msblasa@gmail.com>
2017-11-07 20:44:57 -0800

    working copy

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
 
-app.set('view engine', 'ejs');
+const apiKey = 'db0d02be7935a6bdd3d3bba84ea28d54';
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
-app.get('/', function (req, res){
-    res.render('index');
-});
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
 
-app.listen(3001, function() {
-    console.log('Example app listening on port 3001!');
-});
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
+})
